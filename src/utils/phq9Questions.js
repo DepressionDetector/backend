28b8c4d3b15ed86a4801cfd3413131403bfa4dd4
ImashaNawodi <imashanaw1999@gmail.com>
2025-09-06 12:49:29 +0530

    helpers added

---
+++ src/utils/phq9Questions.js
@@ -0,0 +1,11 @@
+module.exports = [
+    { id: 1, text: "Little interest or pleasure in doing things" },
+    { id: 2, text: "Feeling down, depressed, or hopeless" },
+    { id: 3, text: "Trouble falling or staying asleep, or sleeping too much" },
+    { id: 4, text: "Feeling tired or having little energy" },
+    { id: 5, text: "Poor appetite or overeating" },
+    { id: 6, text: "Feeling bad about yourself — or that you are a failure or have let yourself or your family down" },
+    { id: 7, text: "Trouble concentrating on things, such as reading the newspaper or watching TV" },
+    { id: 8, text: "Moving or speaking slowly, or being fidgety or restless" },
+    { id: 9, text: "Thoughts that you would be better off dead or of hurting yourself" }
+];
